747bda9ad77f9d9bacc1151c8fb076bc5c48a52e
Richa-Ranjan <ranjanricha975@gmail.com>
2026-02-13 21:43:42 +0530

    Add Best Time to Buy and Sell Stock solution

---
+++ buyandsellstocks.java
@@ -0,0 +1,28 @@
+public class buyandsellstocks{
+
+    public static int maxProfit(int[] prices) {
+        // Edge case
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+     
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+
+        return maxProfit;
+    }
+
+   
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int result = maxProfit(prices);
+        System.out.println("Maximum Profit: " + result);
+    }
+}
